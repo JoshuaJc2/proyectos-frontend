9bd0172a36a2ded8927e484c4d46515e97a09529
Joshua Cruz <111657952+JoshuaJc2@users.noreply.github.com>
2024-09-27 15:35:40 -0600

    Apply suggestions from code review
    Co-authored-by: Mónica Miranda <53272126+monmm@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { CategoryComponent } from './modules/product/component/category/category.component';
 
 export const routes: Routes = [
-    {path: "", component: CategoryComponent}
+    {path: "categoria", component: CategoryComponent} // también puede se "category". Según como quieras mantener tus rutas: en español o en inglés.
 ]
