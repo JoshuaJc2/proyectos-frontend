a8c6f36eebc1ed436d1dd5b772c67897e958f333
JoshuaJc2 <Joshua-J-C@ciencias.unam.mx>
2024-11-05 14:43:41 -0600

    Estilos sobre tablas

--- src/app/modules/layout/component/main/main.component.ts
+++ src/app/modules/layout/component/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from '../../../product/component/product/product.component';
+import { ProductService } from '../../../product/_service/product.service';
+import { CategoryService } from '../../../product/_service/category.service';
 
 @Component({
   selector: 'app-main',
@@ -9,4 +11,19 @@ import { ProductComponent } from '../../../product/component/product/product.com
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  category_id : number = 0;
+
+  constructor(
+    private productos : ProductComponent,
+    private productService : ProductService,
+    private categoryService : CategoryService
+  ){}
+
+  ngOnInit(){
+    this.getProductosPorCategoria();
+  }
+
+  getProductosPorCategoria(){
+
+  }
 }
